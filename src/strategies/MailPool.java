dea03e4b2b0c0df1ebf84982ecf3d7d66917d3a0
Gieester Thai <gieestert@student.unimelb.edu.au>
2020-05-04 19:53:33 +1000

    fixed early returns

--- src/strategies/MailPool.java
+++ src/strategies/MailPool.java
@@ -15,13 +15,13 @@ public class MailPool implements IMailPool {
 	private class Item {
 		int destination;
 		MailItem mailItem;
-		
+
 		public Item(MailItem mailItem) {
 			destination = mailItem.getDestFloor();
 			this.mailItem = mailItem;
 		}
 	}
-	
+
 	public class ItemComparator implements Comparator<Item> {
 		@Override
 		public int compare(Item i1, Item i2) {
@@ -34,7 +34,7 @@ public class MailPool implements IMailPool {
 			return order;
 		}
 	}
-	
+
 	private LinkedList<Item> pool;
 	private LinkedList<Robot> robots;
 
@@ -49,17 +49,17 @@ public class MailPool implements IMailPool {
 		pool.add(item);
 		pool.sort(new ItemComparator());
 	}
-	
+
 	@Override
 	public void step() throws ItemTooHeavyException, BreakingFragileItemException {
 		try{
 			ListIterator<Robot> i = robots.listIterator();
 			while (i.hasNext()) loadRobot(i);
-		} catch (Exception e) { 
-            throw e; 
-        } 
+		} catch (Exception e) {
+            throw e;
+        }
 	}
-	
+
 	private void loadRobot(ListIterator<Robot> i) throws ItemTooHeavyException, BreakingFragileItemException {
 		// Cast every robot to caution robot
 		CautionRobot robot = (CautionRobot) i.next();
@@ -72,32 +72,49 @@ public class MailPool implements IMailPool {
 
 				// If it's a fragile item then add to the special arm
 				if (nextItem.getFragile()) {
-
 					robot.addToArm(nextItem);
 					j.remove();
-					nextItem = null;
-					if (pool.size() > 0) { nextItem = j.next().mailItem; }
-				}
-
-				if (nextItem != null) {
-					if (nextItem.getFragile()) { return; }
-					robot.addToHand(nextItem); // hand first as we want higher priority delivered first
-					j.remove();
-
-					if (pool.size() > 0) { nextItem = j.next().mailItem; }
-
-					if (nextItem.getFragile()) { return; }
 
 					if (pool.size() > 0) {
-						robot.addToTube(nextItem);
-						j.remove();
+						nextItem = j.next().mailItem;
+						// Cannot carry more than one fragile item at a time
+						if (nextItem.getFragile()) {
+							robot.dispatch();
+							i.remove();
+							return;
+						}
 					}
-					robot.dispatch(); // send the robot off if it has any items to deliver
+					else {
+						robot.dispatch();
+						i.remove();
+						return;
+					}
+				}
+
+				robot.addToHand(nextItem); // hand first as we want higher priority delivered first
+				j.remove();
+
+				if (pool.size() > 0) {
+					nextItem = j.next().mailItem;
+					// Cannot carry more than one fragile item at a time
+					if (nextItem.getFragile()) {
+						robot.dispatch();
+						i.remove();
+						return;
+					}
+				} else {
+					robot.dispatch();
 					i.remove();
-				}// remove from mailPool queue
-			} catch (Exception e) { 
-	            throw e; 
-	        } 
+					return;
+				}
+
+				robot.addToTube(nextItem);
+				j.remove();
+				robot.dispatch(); // send the robot off if it has any items to deliver
+				i.remove(); // remove from mailPool queue
+			} catch (Exception e) {
+	            throw e;
+	        }
 		}
 	}
 
